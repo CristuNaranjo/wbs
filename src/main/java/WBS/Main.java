9af4f3f565ee48c392cfa6eb809bdacc61469e42
Cristu Naranjo <cristunaranjo@gmail.com>
2016-08-08 14:06:41 +0200

    Aug 8 commit

--- src/main/java/WBS/Main.java
+++ src/main/java/WBS/Main.java
@@ -5,14 +5,19 @@ package WBS;
 
 
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
-import org.apache.spark.ml.classification.LogisticRegression;
-import org.apache.spark.ml.classification.LogisticRegressionModel;
-import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.mllib.regression.IsotonicRegression;
+import org.apache.spark.mllib.regression.IsotonicRegressionModel;
+import scala.Tuple3;
+import scala.Tuple2;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,8 +34,84 @@ public class Main {
                 .getOrCreate();
 
         Dataset<Row> df = spark.read().json("/home/cristu/Proyectos/BigData/src/main/resources/social_reviews.json");
-        Dataset<Row> pos = df.select("positive");
+        Dataset<Row> pos = df.select("id", "positive");
+
+        JavaRDD<Row> rdd = pos.toJavaRDD();
+        JavaRDD<Tuple3<Double, Double, Double>> parsedData = rdd.map(
+                new Function<Row, Tuple3<Double, Double, Double>>() {
+                    public Tuple3<Double, Double, Double> call(Row line) {
+//                        System.out.println(line.getAs("positive").toString());
+//                        String[] parts = {line.getAs("positive").toString(),line.getAs("id").toString()};
+//                        System.out.println(line);
+//                        System.out.println(line.mkString(",").split(","));
+//                        String[] parts = line.mkString(",").split(",");
+                        if(line.isNullAt(0) || line.isNullAt(1)){
+                            return new Tuple3<>(0.0, 0.0, 0.0);
+                        }
+                        return new Tuple3<>(new Double(line.getString(1)), new Double(line.getString(0)), 1.0);
+//                        return new Tuple3<>(new Double(parts[0]), new Double(parts[1]), 1.0);
+//                        return new Tuple3<>(new Double(1.0), new Double(2.0), 1.0);
+                    }
+                }
+        );
+        // Split data into training (60%) and test (40%) sets.
+        JavaRDD<Tuple3<Double, Double, Double>>[] splits = parsedData.randomSplit(new double[]{0.6, 0.4}, 11L);
+        JavaRDD<Tuple3<Double, Double, Double>> training = splits[0];
+        JavaRDD<Tuple3<Double, Double, Double>> test = splits[1];
+
+        // Create isotonic regression model from training data.
+        // Isotonic parameter defaults to true so it is only shown for demonstration
+        final IsotonicRegressionModel model = new IsotonicRegression().setIsotonic(true).run(training);
+        // Create tuples of predicted and real labels.
+        JavaPairRDD<Double, Double> predictionAndLabel = test.mapToPair(
+                new PairFunction<Tuple3<Double, Double, Double>, Double, Double>() {
+                    @Override
+                    public Tuple2<Double, Double> call(Tuple3<Double, Double, Double> point) {
+                        Double predictedLabel = model.predict(point._2());
+                        return new Tuple2<Double, Double>(predictedLabel, point._1());
+                    }
+                }
+        );
+
+        // Calculate mean squared error between predicted and real labels.
+        Double meanSquaredError = new JavaDoubleRDD(predictionAndLabel.map(
+                new Function<Tuple2<Double, Double>, Object>() {
+                    @Override
+                    public Object call(Tuple2<Double, Double> pl) {
+                        return Math.pow(pl._1() - pl._2(), 2);
+                    }
+                }
+        ).rdd()).mean();
+        System.out.println("Mean Squared Error = " + meanSquaredError);
+
+
+//        LinearRegression lr = new LinearRegression()
+//                .setMaxIter(10)
+//                .setRegParam(0.3)
+//                .setElasticNetParam(0.8);
+//        // Fit the model.
+//        LinearRegressionModel lrModel = lr.fit(pos);
+//
+//        // Print the coefficients and intercept for linear regression.
+//        System.out.println("Coefficients: "
+//                + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+
+
+//        JavaRDD<Row> test = pos.javaRDD();
+//        JavaRDD
+
+//        JavaRDD<LabeledPoint> parsedData = pos.map(
+//                new Function<Row, LabeledPoint>() {
+//                    public LabeledPoint call(Row){
+//
+//                        return new LabeledPoint();
+//                    }
+//                }
+//        );
+
         pos.show();
+//        df.show();
+
 
 //        LogisticRegression lr = new LogisticRegression().setMaxIter(1000);
 
---
+++ src/main/java/WBS/Test.java
@@ -0,0 +1,57 @@
+package WBS;
+
+
+/**
+ * Created by cristu on 8/08/16.
+ */
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Spark");
+        SparkConf conf = new SparkConf().setAppName("Test").setMaster("local[4]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Test-SQL")
+                .master("local[4]")
+                .getOrCreate();
+        // Load training data.
+        Dataset<Row> training = spark.read().format("libsvm")
+                .load("/home/cristu/Proyectos/BigData/src/main/resources/data.txt");
+
+        training.show();
+//        LinearRegression lr = new LinearRegression()
+//                .setMaxIter(10)
+//                .setRegParam(0.3)
+//                .setElasticNetParam(0.8);
+//
+//        // Fit the model.
+//        LinearRegressionModel lrModel = lr.fit(training);
+//
+//        // Print the coefficients and intercept for linear regression.
+//        System.out.println("Coefficients: "
+//                + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+//
+//        // Summarize the model over the training set and print out some metrics.
+//        LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+//        System.out.println("numIterations: " + trainingSummary.totalIterations());
+//        System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+//        trainingSummary.residuals().show();
+//        System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+//        System.out.println("r2: " + trainingSummary.r2());
+    }
+}
