7db549306d64a7d8eef606d40076f0d9d1edbb0b
Cristu Naranjo <cristunaranjo@gmail.com>
2016-08-18 11:34:19 +0200

    New Advances!

--- src/main/java/WBS/MoreTests.java
+++ src/main/java/WBS/MoreTests.java
@@ -1,5 +1,6 @@
 package WBS;
 
+import org.apache.commons.math3.util.Precision;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -8,16 +9,25 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.ml.feature.LabeledPoint;
+import org.apache.spark.ml.linalg.VectorUDT;
 import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.regression.GeneralizedLinearRegression;
 import org.apache.spark.ml.regression.GeneralizedLinearRegressionModel;
+import org.apache.spark.ml.regression.GeneralizedLinearRegressionTrainingSummary;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
 import org.apache.spark.util.LongAccumulator;
 import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.spark.sql.types.DataTypes.*;
 
 /**
  * Created by cristu on 17/08/16.
@@ -39,50 +49,55 @@ public class MoreTests {
 //    HOME
 //        Dataset<Row> df = spark.read().json("/Users/NaranjO/Documents/TFG/IntelliJ/wbs/src/main/resources/social_reviews.json");
 
-        Dataset<Row> dfrows = df.select("id", "positive", "user_id", "neutral", "negative")
+        Dataset<Row> dfrows = df.select("id", "user_id", "positive", "neutral", "negative")
                 .filter(new FilterFunction<Row>() {
                     @Override
                     public boolean call(Row row) throws Exception {
                         Double x = 0.0;
                         if (!row.isNullAt(0)) {
-                            x = new Double(row.getString(2));
+                            x = new Double(row.getString(1));
                         }
-                        return !row.isNullAt(0) && x == 36.0;
+                        return !row.isNullAt(0) && x == 38.0;
                     }
                 });
         LongAccumulator accum = sc.sc().longAccumulator();
 
-        Dataset<Row> data = dfrows.map(new MapFunction<Row, Row>() {
-            @Override
-            public Row call(Row row) throws Exception {
-                long count = 1;
-                accum.add(count);
-                return RowFactory.create(accum.value(), new Double(row.getString(1)),);
-            }
+        List<Row> listdf = dfrows.collectAsList();
+        List<Row> mylist = new ArrayList<Row>();
+        for(int i=0; i< listdf.size(); i++){
+//            Row temp;
+//            if(i<listdf.size()){
+//                temp = RowFactory.create(Vectors.dense(new Double(i)), new Integer(listdf.get(i).getString(1)), new Double(listdf.get(i).getString(2)),new Double(listdf.get(i).getString(3)),new Double(listdf.get(i).getString(4)));
+//            }else{
+//                temp = RowFactory.create(Vectors.dense(new Double(i)), new Integer(listdf.get(i-100).getString(1)),1.0,1.0,1.0);
+//            }
+//            mylist.add(temp);
+            mylist.add(RowFactory.create(Vectors.dense(new Double(i)), new Integer(listdf.get(i).getString(1)), new Double(listdf.get(i).getString(2)),new Double(listdf.get(i).getString(3)),new Double(listdf.get(i).getString(4))));
+        }
+        StructType schema = new StructType(new StructField[]{
+                createStructField("features", new VectorUDT(), false, Metadata.empty()),
+                createStructField("user_id", IntegerType, false),
+                createStructField("positive", DoubleType, false),
+                createStructField("neutral", DoubleType, false),
+                createStructField("negative", DoubleType, false)
         });
+        Dataset<Row> data = spark.createDataFrame(mylist, schema);
 
+        data.show();
 
+        Dataset<Row>[] splits = data.randomSplit(new double[]{0.8, 0.2});
+        Dataset<Row> trainingData = splits[0];
+        Dataset<Row> testDatab = splits[1];
 
+        long numData = data.count();
 
-        Dataset<Row> datapos = spark.createDataFrame(posrdd, LabeledPoint.class);
-        Dataset<Row> dataneu = spark.createDataFrame(neurdd, LabeledPoint.class);
-        Dataset<Row> dataneg = spark.createDataFrame(negrdd, LabeledPoint.class);
-
-        System.out.println("Numero de datos positivos: " + datapos.count());
-        System.out.println("Numero de datos neutrales: " + dataneu.count());
-        System.out.println("Numero de datos negativos: " + dataneg.count());
-
-
-        Dataset<Row>[] splits = datapos.randomSplit(new double[]{0.7, 0.3});
-        Dataset<Row> trainingDataPos = splits[0];
-        Dataset<Row> testDataPos = splits[1];
-
-        Dataset<Row>[] splits1 = dataneu.randomSplit(new double[]{0.7, 0.3});
-        Dataset<Row> trainingDataNeu = splits1[0];
-        Dataset<Row> testDataNeu = splits1[1];
-        Dataset<Row>[] splits2 = dataneg.randomSplit(new double[]{0.7, 0.3});
-        Dataset<Row> trainingDataNeg = splits2[0];
-        Dataset<Row> testDataNeg = splits2[1];
+        List<Row> testList = testDatab.collectAsList();
+        List<Row> myTestList = new ArrayList<Row>();
+        myTestList.addAll(testList);
+        for(int i=0; i<10; i++){
+            myTestList.add(RowFactory.create(Vectors.dense(new Double(numData+i)), myTestList.get(0).getInt(1),0.0,0.0,0.0));
+        }
+        Dataset<Row> testData = spark.createDataFrame(myTestList, schema);
 
 
         GeneralizedLinearRegression glr = new GeneralizedLinearRegression()
@@ -91,41 +106,26 @@ public class MoreTests {
                 .setMaxIter(100)
                 .setRegParam(0);
 
-
-        // We may alternatively specify parameters using a ParamMap.
         ParamMap paramMapPos = new ParamMap()
                 .put(glr.labelCol().w("positive"))
-                .put(glr.predictionCol().w("predictionPos"))
-                .put(glr.featuresCol().w("id"));
+                .put(glr.predictionCol().w("predictionPos"));
         ParamMap paramMapNeu = new ParamMap()
                 .put(glr.labelCol().w("neutral"))
-                .put(glr.predictionCol().w("predictionNeu"))
-                .put(glr.featuresCol().w("id"));
+                .put(glr.predictionCol().w("predictionNeu"));
         ParamMap paramMapNeg = new ParamMap()
                 .put(glr.labelCol().w("negative"))
-                .put(glr.predictionCol().w("predictionNeg"))
-                .put(glr.featuresCol().w("id"));
+                .put(glr.predictionCol().w("predictionNeg"));
 
-        // One can also combine ParamMaps.
-
-        ParamMap paramMapCombined = paramMapPos.$plus$plus(paramMapNeu).$plus$plus(paramMapNeg);
-
-        // Now learn a new model using the paramMapCombined parameters.
-        // paramMapCombined overrides all parameters set earlier via lr.set* methods.
-//        LogisticRegressionModel model2 = lr.fit(training, paramMapCombined);
-//        System.out.println("Model 2 was fit using parameters: " + model2.parent().extractParamMap());
-
-        // Fit the model
-        GeneralizedLinearRegressionModel modelPos = glr.fit(trainingDataPos);
-        GeneralizedLinearRegressionModel modelNeu = glr.fit(trainingDataNeu);
-        GeneralizedLinearRegressionModel modelNeg = glr.fit(trainingDataNeg);
-
+//        // Fit the model
+        GeneralizedLinearRegressionModel modelPos = glr.fit(trainingData,paramMapPos);
+        GeneralizedLinearRegressionModel modelNeu = glr.fit(trainingData,paramMapNeu);
+        GeneralizedLinearRegressionModel modelNeg = glr.fit(trainingData,paramMapNeg);
 
 //        // Print the coefficients and intercept for generalized linear regression model
 //        System.out.println("Coefficients: " + model.coefficients());
 //        System.out.println("Intercept: " + model.intercept());
-//
-//        // Summarize the model over the training set and print out some metrics
+////
+////        // Summarize the model over the training set and print out some metrics
 //        GeneralizedLinearRegressionTrainingSummary summary = model.summary();
 //        System.out.println("Coefficient Standard Errors: "
 //                + Arrays.toString(summary.coefficientStandardErrors()));
@@ -140,20 +140,42 @@ public class MoreTests {
 //        System.out.println("Deviance Residuals: ");
 //        summary.residuals().show();
 
-        Dataset<Row> resultsPos = modelPos.transform(testDataPos).toDF("feautures", "labelPos", "predictionPos");
-        Dataset<Row> resultsNeu = modelNeu.transform(testDataNeu).toDF("feautures", "labelNeu", "predictionNeu");
-        Dataset<Row> resultsNeg = modelNeg.transform(testDataNeg).toDF("feautures", "labelNeg", "predictionNeg");
+        Dataset<Row> resultsPos = modelPos.transform(testData);
+        Dataset<Row> resultsNeu = modelNeu.transform(testData);
+        Dataset<Row> resultsNeg = modelNeg.transform(testData);
 
-        resultsPos.createOrReplaceTempView("resultPos");
-        resultsNeu.createOrReplaceTempView("resultNeu");
-        resultsNeg.createOrReplaceTempView("resultNeg");
+        List<Row> listPos = resultsPos.collectAsList();
+        List<Row> listNeu = resultsNeu.collectAsList();
+        List<Row> listNeg = resultsNeg.collectAsList();
+
+        List<Row> resultsList = new ArrayList<Row>();
+        for(int i=0; i< listPos.size(); i++){
+            double sumPred = Precision.round(listNeg.get(i).getDouble(5)+listNeu.get(i).getDouble(5)+listPos.get(i).getDouble(5),3);
+            resultsList.add(RowFactory.create(listPos.get(i).get(0), listPos.get(i).get(1), listPos.get(i).get(2), listPos.get(i).get(3), listPos.get(i).get(4), listPos.get(i).get(5), listNeu.get(i).get(5), listNeg.get(i).get(5),sumPred));
+        }
+        StructType schemaResults = new StructType(new StructField[]{
+                createStructField("features", new VectorUDT(), false, Metadata.empty()),
+                createStructField("user_id", IntegerType, false),
+                createStructField("positive", DoubleType, false),
+                createStructField("neutral", DoubleType, false),
+                createStructField("negative", DoubleType, false),
+                createStructField("predPositive", DoubleType, false),
+                createStructField("predNeutral", DoubleType, false),
+                createStructField("predNegative", DoubleType, false),
+                createStructField("SumPred", DoubleType, false)
+        });
+        Dataset<Row> results = spark.createDataFrame(resultsList, schemaResults);
 
 
+        results.show();
 
+//        resultsPos.show();
+//        resultsNeu.show();
+//        resultsNeg.show();
 
-        resultsPos.show();
-        resultsNeu.show();
-        resultsNeg.show();
+        for (Row r: results.collectAsList()) {
+            System.out.println("Comment-> " + r.get(0) + " , predicPositive = " + r.get(5) + " , predicNeutral = " + r.get(6) + " , predicNegative = " + r.get(7) + " , SumPredict = " + r.get(8) );
+        }
 
     }
 }
